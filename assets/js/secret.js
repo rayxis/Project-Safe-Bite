f12d3631b33b934f401f6a0d01ff86bf65de4ec5
Ray <ray@rayxis.com>
2024-01-16 16:10:15 -0600

    Removed Edamam. Added code to view and store the recipe data.

--- assets/js/secret.js
+++ assets/js/secret.js
@@ -1,11 +1,6 @@
 const apiKeys = {
 	// https://api-ninjas.com/api
 	apiNinjas: 'k5yif66Z66/38hmt873yfg==ZO2R3SNz5FDWtDwM',
-	// https://developer.edamam.com/edamam-docs-nutrition-api
-	edamam: {
-		app_id: 'c131f255',
-		app_key: '72b045987fac7f55d3a200703da7b27b'
-	},
 	// https://spoonacular.com/food-api
 	spoonacular: '82fefa954e6d4b74a0335fd10cb3b3ff',
 };
